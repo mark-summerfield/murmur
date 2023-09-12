d1f1acd52cb7121d1680fbac6b4e172501c6932d
mark-summerfield <mark@qtrac.eu>
2023-09-12 13:30:37 +0100

    Command items

--- item.go
+++ item.go
@@ -18,4 +18,24 @@ func (me Value) String() string { return strconv.Itoa(int(me)) }
 type Label string
 
 func (me Label) Value() int     { return -222 }
-func (me Label) String() string { return string(me) }
+func (me Label) String() string { return string(me) + ":" }
+
+type CopyCommand string
+
+func (me CopyCommand) Value() int     { return cmdCopy }
+func (me CopyCommand) String() string { return "C" }
+
+type JumpCommand string
+
+func (me JumpCommand) Value() int     { return cmdJump }
+func (me JumpCommand) String() string { return "J" }
+
+type SuccCommand string
+
+func (me SuccCommand) Value() int     { return cmdSucc }
+func (me SuccCommand) String() string { return "S" }
+
+type ZeroCommand string
+
+func (me ZeroCommand) Value() int     { return cmdZero }
+func (me ZeroCommand) String() string { return "Z" }
